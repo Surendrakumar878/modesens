ab66a94d8b42aeb8379fe8c8a64d133366d93613
Randeep Singh <randeepsingh986@gmail.com>
2022-12-13 04:23:28 +0530

    added navbar

--- hissing-love-5128/src/App.js
+++ hissing-love-5128/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { MainRoutes } from './page/MainRoutes';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     <MainRoutes/>
     </div>
   );

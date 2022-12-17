79130153abec4b5cf73f1b1dabe15b4205f30cea
unknown <surendramarshkole78@gmail.com>
2022-12-17 17:12:33 +0530

    aa

--- hissing-love-5128/src/page/HomePage.jsx
+++ hissing-love-5128/src/page/HomePage.jsx
@@ -21,7 +21,7 @@ export const HomePage = () => {
     }, [])
     console.log(data)
   return (
-    <Box>
+    <Box pos="ralative" w={{lg:"100%" ,md:"100%", sm:"100%"}} >
       <Box>
         <Carousel>
           <Carousel.Item interval={1000}>
@@ -62,7 +62,7 @@ export const HomePage = () => {
         </Carousel>
       </Box>
       <Box textAlign={"center"}>
-        <Heading >New Partners and Designers</Heading>
+        <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>New Partners and Designers</Heading>
         <Box>
           Check out the new retail partners and brands who just joined the
           ModeSens family.
@@ -76,10 +76,10 @@ export const HomePage = () => {
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
+                <Text fontSize={{lg:"20px",md:"20px",sm:"10px"}} >First slide label</Text>
+                <Text>
                   Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
+                </Text>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={500}>
@@ -112,19 +112,31 @@ export const HomePage = () => {
       </Box>
       <Box>
         <Box textAlign={"center"}>
-          <Heading>Shop Women</Heading>
-          <Wrap  boxShadow='dark-lg' pt="10px"  pb="10px"   bg='white'   h="400px" w="1150px" ml="100px" display={"flex"} justifyContent="space-around"  spacing={7} >
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Shop Women</Heading>
+          <Wrap  boxShadow='dark-lg' pt={{lg:"10px",md:"10px",sm:"10px"}} pb={{lg:"10px",md:"10px",sm:"10px"}}   bg='white'   h={{lg:"400px",md:"400px",sm:"400px"}} w={{lg:"100%",sm:'100%',md:"100%"}} ml={{lg:"100px",md:"50px",sm:"25%"}} display={"flex"} justifyContent="space-around"  spacing={7} >
 {womens.map((i)=>{
-    return <Link to={`/details/${i.id}`}> <WrapItem  mb="110px" ml="10px"  w='195px' h='auto' border={"1px solid red"} key={i.id} display="flex"
+    return <Link to={`/details/${i.id}`}> <WrapItem  mb={{lg:"110px",md:"110px",sm:"110px"}} ml={{lg:"10px",md:"10px",sm:"10px"}}  w={{lg:'195px',md:"195px"}} h={{lg:'auto',md:"auto",sm:"auto"}} border={"1px solid red"} key={i.id} display="flex"
   flexDirection={"column"}  justifyContent="center"    > 
-
-<Box border={"1px solid red"} h="250px" w="100%" >
+<Box position="relative" right="-140px" >
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="heartsvg"
+          width="20"
+          height="20"
+          style={{ marginBottom: "10px", marginTop: "5px" }}
+          fill="gray"
+          viewBox="0 0 512 512"
+        >
+          <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+        </svg>
+  </Box>
+<Box border={"1px solid red"} h={{lg:"250px",md:"250px",sm:"250px"}} w={{lg:"100%",sm:'100%',md:"100%"}} >
   
-  <Image w="70%" src={i.image} alt="" />
+  <Image w={{lg:"70%",md:"70%",sm:"70%"}} src={i.image} alt="" />
   </Box> 
-  <Box border={"1px solid red"} h="100px" w="100%" >
+  <Box border={"1px solid red"} h={{lg:"100px",md:"70px"}} w={{lg:"100%",md:"100%",sm:"100%"}} fontSize={{lg:"20px",md:"20px",sm:"20px"}} >
 
-    <Text>{i.title}</Text> 
+    <Text >{i.title}</Text> 
   </Box>
    
     
@@ -135,19 +147,31 @@ export const HomePage = () => {
       </Box>
       <Box textAlign={"center"}>
         <Box>
-          <Heading>New To sale</Heading>
-          <Wrap  boxShadow='dark-lg' pt="10px"  pb="10px"   bg='white'   h="400px" w="1150px" ml="100px" display={"flex"} justifyContent="space-around"  spacing={7} >
-{data.map((i)=>{
-    return <Link to={`/details/${i.id}`}> <WrapItem  mb="110px" ml="10px"  w='195px' h='auto' border={"1px solid red"} key={i.id} display="flex"
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>New To sale</Heading>
+          <Wrap  boxShadow='dark-lg' pt={{lg:"10px",md:"10px",sm:"10px"}} pb={{lg:"10px",md:"10px",sm:"10px"}}   bg='white'   h={{lg:"400px",md:"400px",sm:"400px"}} w={{lg:"100%",sm:'100%',md:"100%"}} ml={{lg:"100px",md:"50px",sm:"25%"}} display={"flex"} justifyContent="space-around"  spacing={7} >
+{womens.map((i)=>{
+    return <Link to={`/details/${i.id}`}> <WrapItem  mb={{lg:"110px",md:"110px",sm:"110px"}} ml={{lg:"10px",md:"10px",sm:"10px"}}  w={{lg:'195px',md:"195px"}} h={{lg:'auto',md:"auto",sm:"auto"}} border={"1px solid red"} key={i.id} display="flex"
   flexDirection={"column"}  justifyContent="center"    > 
-
-<Box border={"1px solid red"} h="250px" w="100%" >
+<Box position="relative" right="-140px" >
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="heartsvg"
+          width="20"
+          height="20"
+          style={{ marginBottom: "10px", marginTop: "5px" }}
+          fill="gray"
+          viewBox="0 0 512 512"
+        >
+          <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+        </svg>
+  </Box>
+<Box border={"1px solid red"} h={{lg:"250px",md:"250px",sm:"250px"}} w={{lg:"100%",sm:'100%',md:"100%"}} >
   
-  <Image w="70%" src={i.image} alt="" />
+  <Image w={{lg:"70%",md:"70%",sm:"70%"}} src={i.image} alt="" />
   </Box> 
-  <Box border={"1px solid red"} h="100px" w="100%" >
+  <Box border={"1px solid red"} h={{lg:"100px",md:"70px"}} w={{lg:"100%",md:"100%",sm:"100%"}} fontSize={{lg:"20px",md:"20px",sm:"20px"}} >
 
-    <Text>{i.title}</Text> 
+    <Text >{i.title}</Text> 
   </Box>
    
     
@@ -159,19 +183,31 @@ export const HomePage = () => {
       </Box>
       <Box textAlign={"center"}>
         <Box>
-          <Heading>Special Offers</Heading>
-          <Wrap  boxShadow='dark-lg' pt="10px"  pb="10px"   bg='white'   h="400px" w="1150px" ml="100px" display={"flex"} justifyContent="space-around"  spacing={7} >
-{special.map((i)=>{
-    return <Link to={`/details/${i.id}`}> <WrapItem  mb="110px" ml="10px"  w='195px' h='auto' border={"1px solid red"} key={i.id} display="flex"
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Special Offers</Heading>
+          <Wrap  boxShadow='dark-lg' pt={{lg:"10px",md:"10px",sm:"10px"}} pb={{lg:"10px",md:"10px",sm:"10px"}}   bg='white'   h={{lg:"400px",md:"400px",sm:"400px"}} w={{lg:"100%",sm:'100%',md:"100%"}} ml={{lg:"100px",md:"50px",sm:"25%"}} display={"flex"} justifyContent="space-around"  spacing={7} >
+{womens.map((i)=>{
+    return <Link to={`/details/${i.id}`}> <WrapItem  mb={{lg:"110px",md:"110px",sm:"110px"}} ml={{lg:"10px",md:"10px",sm:"10px"}}  w={{lg:'195px',md:"195px"}} h={{lg:'auto',md:"auto",sm:"auto"}} border={"1px solid red"} key={i.id} display="flex"
   flexDirection={"column"}  justifyContent="center"    > 
-
-<Box border={"1px solid red"} h="250px" w="100%" >
+<Box position="relative" right="-140px" >
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="heartsvg"
+          width="20"
+          height="20"
+          style={{ marginBottom: "10px", marginTop: "5px" }}
+          fill="gray"
+          viewBox="0 0 512 512"
+        >
+          <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+        </svg>
+  </Box>
+<Box border={"1px solid red"} h={{lg:"250px",md:"250px",sm:"250px"}} w={{lg:"100%",sm:'100%',md:"100%"}} >
   
-  <Image w="60%" src={i.image} alt="" />
+  <Image w={{lg:"70%",md:"70%",sm:"70%"}} src={i.image} alt="" />
   </Box> 
-  <Box border={"1px solid red"} h="100px" w="100%" >
+  <Box border={"1px solid red"} h={{lg:"100px",md:"70px"}} w={{lg:"100%",md:"100%",sm:"100%"}} fontSize={{lg:"20px",md:"20px",sm:"20px"}} >
 
-    <Text>{i.title}</Text> 
+    <Text >{i.title}</Text> 
   </Box>
    
     
@@ -183,19 +219,31 @@ export const HomePage = () => {
       </Box>
       <Box textAlign={"center"}>
         <Box>
-          <Heading>Trending Now</Heading>
-          <Wrap  boxShadow='dark-lg' pt="10px"  pb="10px"   bg='white'   h="400px" w="1150px" ml="100px" display={"flex"} justifyContent="space-around"  spacing={7} >
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Trending Now</Heading>
+          <Wrap  boxShadow='dark-lg' pt={{lg:"10px",md:"10px",sm:"10px"}} pb={{lg:"10px",md:"10px",sm:"10px"}}   bg='white'   h={{lg:"400px",md:"400px",sm:"400px"}} w={{lg:"100%",sm:'100%',md:"100%"}} ml={{lg:"100px",md:"50px",sm:"25%"}} display={"flex"} justifyContent="space-around"  spacing={7} >
 {womens.map((i)=>{
-    return <Link to={`/details/${i.id}`}> <WrapItem  mb="110px" ml="10px"  w='195px' h='auto' border={"1px solid red"} key={i.id} display="flex"
+    return <Link to={`/details/${i.id}`}> <WrapItem  mb={{lg:"110px",md:"110px",sm:"110px"}} ml={{lg:"10px",md:"10px",sm:"10px"}}  w={{lg:'195px',md:"195px"}} h={{lg:'auto',md:"auto",sm:"auto"}} border={"1px solid red"} key={i.id} display="flex"
   flexDirection={"column"}  justifyContent="center"    > 
-
-<Box border={"1px solid red"} h="250px" w="100%" >
+<Box position="relative" right="-140px" >
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="heartsvg"
+          width="20"
+          height="20"
+          style={{ marginBottom: "10px", marginTop: "5px" }}
+          fill="gray"
+          viewBox="0 0 512 512"
+        >
+          <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+        </svg>
+  </Box>
+<Box border={"1px solid red"} h={{lg:"250px",md:"250px",sm:"250px"}} w={{lg:"100%",sm:'100%',md:"100%"}} >
   
-  <Image w="70%" src={i.image} alt="" />
+  <Image w={{lg:"70%",md:"70%",sm:"70%"}} src={i.image} alt="" />
   </Box> 
-  <Box border={"1px solid red"} h="100px" w="100%" >
+  <Box border={"1px solid red"} h={{lg:"100px",md:"70px"}} w={{lg:"100%",md:"100%",sm:"100%"}} fontSize={{lg:"20px",md:"20px",sm:"20px"}} >
 
-    <Text>{i.title}</Text> 
+    <Text >{i.title}</Text> 
   </Box>
    
     
@@ -207,19 +255,31 @@ export const HomePage = () => {
       </Box>
       <Box>
         <Box textAlign={"center"}>
-          <Heading>Recently Viewed</Heading>
-          <Wrap  boxShadow='dark-lg' pt="10px"  pb="10px"   bg='white'   h="400px" w="1150px" ml="100px" display={"flex"} justifyContent="space-around"  spacing={7} >
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Recently Viewed</Heading>
+          <Wrap  boxShadow='dark-lg' pt={{lg:"10px",md:"10px",sm:"10px"}} pb={{lg:"10px",md:"10px",sm:"10px"}}   bg='white'   h={{lg:"400px",md:"400px",sm:"400px"}} w={{lg:"100%",sm:'100%',md:"100%"}} ml={{lg:"100px",md:"50px",sm:"25%"}} display={"flex"} justifyContent="space-around"  spacing={7} >
 {womens.map((i)=>{
-    return <Link to={`/details/${i.id}`}> <WrapItem  mb="110px" ml="10px"  w='195px' h='auto' border={"1px solid red"} key={i.id} display="flex"
+    return <Link to={`/details/${i.id}`}> <WrapItem  mb={{lg:"110px",md:"110px",sm:"110px"}} ml={{lg:"10px",md:"10px",sm:"10px"}}  w={{lg:'195px',md:"195px"}} h={{lg:'auto',md:"auto",sm:"auto"}} border={"1px solid red"} key={i.id} display="flex"
   flexDirection={"column"}  justifyContent="center"    > 
-
-<Box border={"1px solid red"} h="250px" w="100%" >
+<Box position="relative" right="-140px" >
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="heartsvg"
+          width="20"
+          height="20"
+          style={{ marginBottom: "10px", marginTop: "5px" }}
+          fill="gray"
+          viewBox="0 0 512 512"
+        >
+          <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+        </svg>
+  </Box>
+<Box border={"1px solid red"} h={{lg:"250px",md:"250px",sm:"250px"}} w={{lg:"100%",sm:'100%',md:"100%"}} >
   
-  <Image w="70%" src={i.image} alt="" />
+  <Image w={{lg:"70%",md:"70%",sm:"70%"}} src={i.image} alt="" />
   </Box> 
-  <Box border={"1px solid red"} h="100px" w="100%" >
+  <Box border={"1px solid red"} h={{lg:"100px",md:"70px"}} w={{lg:"100%",md:"100%",sm:"100%"}} fontSize={{lg:"20px",md:"20px",sm:"20px"}} >
 
-    <Text>{i.title}</Text> 
+    <Text >{i.title}</Text> 
   </Box>
    
     
@@ -231,38 +291,39 @@ export const HomePage = () => {
       </Box>
       <Box>
         <Box textAlign={"center"}>
-          <Heading>Community Posts</Heading>
-          <Box p='50px' w={"100%"} display={"flex"} justifyContent="space-around" >
-            <Image w="15%" src="https://cdn.modesens.com/umedia/1707011s?w=400" alt="" />
-            <Image w="15%" src="https://cdn.modesens.com/umedia/1707018s?w=400" alt="" />
-            <Image w="15%" src="https://cdn.modesens.com/umedia/1707030s?w=400" alt="" />
-            <Image w="15%" src="https://cdn.modesens.com/umedia/1707141s?w=400" alt="" />
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Community Posts</Heading>
+          <Box p='50px' w={{lg:"100%" ,md:"100%", sm:"100%"}} display={"flex"} justifyContent="space-around" >
+            <Image w={{lg:"15%",md:"15%",sm:"15%"}} src="https://cdn.modesens.com/umedia/1707011s?w=400" alt="" />
+            <Image w={{lg:"15%",md:"15%",sm:"15%"}} src="https://cdn.modesens.com/umedia/1707018s?w=400" alt="" />
+            <Image w={{lg:"15%",md:"15%",sm:"15%"}} src="https://cdn.modesens.com/umedia/1707030s?w=400" alt="" />
+            <Image w={{lg:"15%",md:"15%",sm:"15%"}} src="https://cdn.modesens.com/umedia/1707141s?w=400" alt="" />
           </Box>
           <Button>VIEW ALL</Button>
         </Box>
       </Box>
-      <Box textAlign={"center"} w={{lg:"100%"}} display={"flex"} justifyContent="space-around" >
-        <Box w="30%" alignItems={"center"} >
-          <Heading>Download the ModeSens App</Heading>
-          <Text>A seamless experience that takes your style
+      <Box textAlign={"center"} w={{lg:"100%" ,md:"100%", sm:"100%"}} display={"flex"} justifyContent="space-around" >
+        <Box w={{lg:"30%",md:"30%",sm:"30%"}} alignItems={"center"} >
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Download the ModeSens App</Heading>
+          <Text fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>A seamless experience that takes your style
 to the next level.</Text>
-<Button borderRadius={0} bg={"black"} color="white">DOWNLOAD NOW</Button>
+<Button fontSize= {{lg:"40px",md:"40px",sm:"40px"}} borderRadius={0} bg={"black"} color="white">DOWNLOAD NOW</Button>
 <Center>
 
           <Image  src="https://cdn.modesens.com/static/img/20220826appdownload_en.svg" alt="" />
 </Center>
         </Box>
-        <Box w="30%">
-          <Heading>Download the ModeSens App</Heading>
+        <Box w={{lg:"30%",md:"30%",sm:"30%"}}>
+          <Heading fontSize= {{lg:"40px",md:"40px",sm:"40px"}}>Download the ModeSens App</Heading>
           <Text>Get timely, accurate product information
 every time you browse.</Text>
-<Button borderRadius={0} bg={"black"} color="white">INSTALL NOW</Button>
+<Button fontSize= {{lg:"40px",md:"40px",sm:"40px"}} borderRadius={0} bg={"black"} color="white">INSTALL NOW</Button>
 <Center>
 
           <img src="https://cdn.modesens.com/static/img/20211109downloadright.png" alt="" />
 </Center>
         </Box>
       </Box>
+    
     </Box>
   );
 };

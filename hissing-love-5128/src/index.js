2027f8bb3d3cf214d6f6ebeb9a3441a577483f85
ankit2341 <ankitpatil2341@gmail.com>
2022-12-17 17:08:48 +0530

    fw19_1313_day_5

--- hissing-love-5128/src/index.js
+++ hissing-love-5128/src/index.js
@@ -4,18 +4,19 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import { store,persistor } from "./Redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
 // import { ChakraProvider } from '@chakra-ui/react';
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
  <BrowserRouter>
   <Provider store={store}>
-
+     <PersistGate persistor={persistor}>
 
     <App />
-
+    </PersistGate>
 
     </Provider>
   </BrowserRouter>

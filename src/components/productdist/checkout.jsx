e11c2c205f75b5d2d5207c0de51fdd4444d8c46a
rekha yadav <rekha.ee026@gmail.com>
2023-02-08 13:19:12 +0530

    checkout page added

--- src/components/productdist/checkout.jsx
+++ src/components/productdist/checkout.jsx
@@ -1,28 +1,45 @@
-import { Button, FormLabel, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, FormControl, FormErrorMessage, FormLabel, Input,FormHelperText } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom"
-import "./checkout.css";
+import { checkoutclear } from "../../Redux/ProductReducer/action";
+import "./check.css";
 
 function Tabs() {
+  const cartData = useSelector((store) => store.ProductReducer.CartData);
+  console.log(cartData)
   const [toggleState, setToggleState] = useState(1);
+  const dispatch=useDispatch()
   const navigate = useNavigate()
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [address, setAddress] = useState("")
+  const [pin, setPin] = useState("")
+  const [total, setTotal] = useState(0);
+    useEffect(() => {
+      let sum = 0;
+      for (let i = 0; i < cartData.length; i++) {
+        sum += cartData[i].price;
+      }
+      setTotal(Math.round(sum));
+    }, [cartData]);
+    // console.log(pin.length)
   const handlesubmit = () => {
-    if (name === "" || email === "" || address === "") {
-      alert("fill all input fileds")
-      navigate("/checkout")
-    }
-    else {
+    if(pin.length===6){
+      dispatch(checkoutclear([]));
+      alert("Your order placed Successfully")
       navigate("/")
     }
-
+    else{
+      alert("enter correct credential")
+    }
+    
   }
 
   const toggleTab = (index) => {
     setToggleState(index);
   };
+ 
 
   return (
     <div className="container">
@@ -52,23 +69,24 @@ function Tabs() {
             <div className="cashform">
               <form onSubmit={handlesubmit}>
                 <label>Full Name</label>
-                <Input placeholder="Enter your Full Name" type="text" onChange={(e) => setName(e.target.value)} /><br />
+                <Input placeholder="Enter your Full Name" type="text" value={name}onChange={(e) => setName(e.target.value)} required /><br />
                 <label>Email</label>
-                <Input placeholder="Enter your Email" type="text" onChange={(e) => setEmail(e.target.value)} />
+                <Input placeholder="Enter your Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <label>Address</label>
-                <Input placeholder="Enter your Address" type="text" onChange={(e) => setAddress(e.target.value)} />
+                <Input placeholder="Enter your Address" type="text" value={address}onChange={(e) => setAddress(e.target.value)} required/>
                 <label>Pin</label>
-                <Input placeholder="Enter your Pin" type="text" />
-                <label>Phone no</label>
-                <Input placeholder="Phone no" type="number" />
+                <Input placeholder="Enter your Pin" type="number" value={pin} onChange={(e) => setPin(e.target.value)} />
+              <p style={{color:"red"}}>{pin.length!=6 && pin.length>0?"Enter 6 digit pin":""}</p> 
+               <br/> <label>Phone no</label>
+                <Input placeholder="Phone no" type="number" required/>
                 <Button type="submit" bg={"black"} color="white" _hover={{ bg: 'black' }} w={"100%"} mt={"15px"}>Submit</Button>
               </form>
             </div>
             <div className="grandtotal">
-              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Total</p> <p style={{ fontWeight: "bold", fontSize: "20px" }}> : ₹567</p></div>
+              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Total</p> <p style={{ fontWeight: "bold", fontSize: "20px" }}> : ₹{total}</p></div>
               <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Shipping charges</p><p style={{ fontWeight: "bold", fontSize: "20px" }}>Free</p></div>
 
-              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Your Grand Total</p> <p style={{ fontWeight: "bold", fontSize: "20px", color: "red" }}> : ₹567</p></div>
+              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Your Grand Total</p> <p style={{ fontWeight: "bold", fontSize: "20px", color: "red" }}> : ₹{total}</p></div>
             </div>
           </div>
 
@@ -80,63 +98,49 @@ function Tabs() {
         >
           <h2>Your Shipping Address</h2>
           <hr />
-          {/* <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-            voluptatum qui adipisci.
-          </p> */}
+         
           <div className="maincash">
             <div className="cashform">
               <form onSubmit={handlesubmit}>
                 <label>Full Name</label>
-                <Input placeholder="Enter your Full Name" type="text" onChange={(e) => setName(e.target.value)} /><br />
+                <Input placeholder="Enter your Full Name" type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br />
                 <label>Email</label>
-                <Input placeholder="Enter your Email" type="text" onChange={(e) => setEmail(e.target.value)} />
+                <Input placeholder="Enter your Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <label>Address</label>
-                <Input placeholder="Enter your Address" type="text" onChange={(e) => setAddress(e.target.value)} />
+                <Input placeholder="Enter your Address" type="text" value={address} onChange={(e) => setAddress(e.target.value)} required/>
                 <label>Pin</label>
-                <Input placeholder="Enter your Pin" type="text" />
+                <Input placeholder="Enter your Pin" type="number" value={pin} onChange={(e) => setPin(e.target.value)} />
+              <p style={{color:"red"}}>{pin.length!=6 && pin.length>0?"Enter 6 digit pin":""}</p> 
                 <label>Phone no</label>
-                <Input placeholder="Phone no" type="number" />
+                <Input placeholder="Phone no" type="number" required/>
                 <Button type="submit" bg={"black"} color="white" _hover={{ bg: 'black' }} w={"100%"} mt={"15px"}>Submit</Button>
               </form>
             </div>
             <div className="grandtotal">
+            
               <label>Card Number</label>
-              <Input placeholder="Enter your 16 digit card number" type="number" /><br />
+              <Input placeholder="Enter your 16 digit card number" type="number" required/><br />
               <label >Name on Card</label>
-              <Input placeholder="Name on card" type="number" style={{marginBottom:"20px"}}/><br />
+              <Input placeholder="Name on card" type="text" style={{marginBottom:"20px"}} required/><br />
               <div style={{ display: "flex" }}>
                 <div><label>Expiry Date</label>
-                  <Input placeholder="Expiry" type="text" /></div>
+                  <Input placeholder="Expiry" type="text" required /></div>
                 <div style={{ marginLeft: "20px" }}>
                   <p>CVV</p>
-                  <Input placeholder="CVV" type="number" />
+                  <Input placeholder="CVV" type="password" required/>
                 </div>
 
 
               </div>
-              <div style={{marginTop:"20px"}}><p style={{ fontWeight: "bold", fontSize: "20px" }}>Total</p> <p style={{ fontWeight: "bold", fontSize: "20px" }}> : ₹567</p></div>
+              <div style={{marginTop:"20px"}}><p style={{ fontWeight: "bold", fontSize: "20px" }}>Total</p> <p style={{ fontWeight: "bold", fontSize: "20px" }}> : ₹{total}</p></div>
               <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Shipping charges</p><p style={{ fontWeight: "bold", fontSize: "20px" }}>Free</p></div>
 
-              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Your Grand Total</p> <p style={{ fontWeight: "bold", fontSize: "20px", color: "red" }}> : ₹567</p></div>
+              <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>Your Grand Total</p> <p style={{ fontWeight: "bold", fontSize: "20px", color: "red" }}> : ₹{total}</p></div>
             </div>
           </div>
         </div>
 
-        {/* <div
-          className={toggleState === 3 ? "content  active-content" : "content"}
-        >
-          <h2>Content 3</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-            Accusamus in quia odit aspernatur provident et ad vel distinctio
-            recusandae totam quidem repudiandae omnis veritatis nostrum
-            laboriosam architecto optio rem, dignissimos voluptatum beatae
-            aperiam voluptatem atque. Beatae rerum dolores sunt.
-          </p>
-        </div> */}
+      
       </div>
     </div>
   );
